3f266024c9d69682befbfd2e119f999185c1b596
lh233 <490095224@qq.com>
2020-08-18 10:06:29 +0800

    add update repositories functions

--- update.bat
+++ update.bat
@@ -2,8 +2,10 @@
 @title bat 交互执行git 每日提交命令
 F:
 cd F:\project\mygithub\linux-kernel
+git pull 
 git status .
 git add .
+git status .
 @set /p option="please input y/n to commit??:" 
 
 if "%option%" == "y" (
