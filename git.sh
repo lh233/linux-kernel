10af7ff200798c503ed3e71f3634742a72e3995a
lh233 <490095224@qq.com>
2021-07-22 15:16:01 +0800

    update

--- git.sh
+++ git.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-git add /home/narwal/study/linux-kernel/*
-git commit -m 'update'
-git push origin master
+cd /home/narwal/study/linux-kernel/ && git add /home/narwal/study/linux-kernel/* && git commit -m 'update' && git push origin master && echo "update end " > /home/narwal/study/linux-kernel/1.txt
