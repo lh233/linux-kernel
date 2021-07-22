29e539adcb0ff8e20617f3fad8b722e7b93f60db
lh233 <490095224@qq.com>
2021-07-22 15:05:17 +0800

    update

--- git.sh
+++ git.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-echo "update" > /home/narwal/study/1.txt
-/usr/bin/git add .
-/usr/bin/git commit -m 'update'
-/usr/bin/git push origin master
-echo "update end" >> /home/narwal/1.txt
+git add /home/narwal/study/linux-kernel/*
+git commit -m 'update'
+git push origin master
