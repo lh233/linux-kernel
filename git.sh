7b145f869aa8324fe385d61092e3f565277b914f
lh233 <490095224@qq.com>
2021-07-22 16:02:25 +0800

    add git.sh

--- git.sh
+++ git.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
 project_path="/home/narwal/study/linux-kernel/"
-date="`date +%Y-%m-%d %H:%M:%S`"
+project_date="`date +%y%m%d`"
 cd $project_path
+git pull origin master
 git add $project_path*
-git commit
+git commit -m 'update....'
 git push origin master
 echo "update end " > $project_path/update.log
+echo $project_date >> $project_path/update.log
