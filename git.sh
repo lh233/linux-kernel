e354bafa5f84fffebb2613ddd9d090ecf07ade1e
lh233 <490095224@qq.com>
2021-07-22 14:35:18 +0800

    update

--- git.sh
+++ git.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-echo "hello world" > 1.txt
-git add .
-git commit -m 'update'
-git config user.name 'lh233'
-git config user.email '490095224@qq.com'
-git push origin master
-
+echo "update" > /home/narwal/study/1.txt
+/usr/bin/git add .
+/usr/bin/git commit -m 'update'
+/usr/bin/git push origin master
