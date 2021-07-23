a8b6b118d56c483da8bb1ad7040f0f1e713fa945
lh233 <490095224@qq.com>
2021-07-23 11:21:09 +0800

    update....

--- git.sh
+++ git.sh
@@ -4,9 +4,10 @@ export PATH
 project_path="/home/narwal/study/linux-kernel/"
 project_date="`date +"update time:%Y-%m-%d %H:%M:%S"`"
 cd $project_path
+echo "update" > $project_path/update.log
 git pull origin master
 git add $project_path*
 git commit -m 'update....'
 git push origin master
-echo "update end " > $project_path/update.log
+echo "update end " >> $project_path/update.log
 echo $project_date >> $project_path/update.log
